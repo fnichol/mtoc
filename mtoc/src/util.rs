2a228882d7386e9c8373c737961213d598c500fc
Fletcher Nichol <fnichol@nichol.ca>
2019-06-18 16:24:16 -0600

    [mtoc/style] Favor an if to single arm match
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- mtoc/src/util.rs
+++ mtoc/src/util.rs
@@ -76,19 +76,16 @@ pub(crate) fn setup_panic_hooks() {
 
     let default_hook = panic::take_hook();
 
-    match env::var("RUST_BACKTRACE") {
-        Err(_) => {
-            panic::set_hook(Box::new(move |info: &panic::PanicInfo| {
-                // First call the default hook that prints to standard error
-                default_hook(info);
+    if env::var("RUST_BACKTRACE").is_err() {
+        panic::set_hook(Box::new(move |info: &panic::PanicInfo| {
+            // First call the default hook that prints to standard error
+            default_hook(info);
 
-                // Then call human_panic
-                let file_path = human_panic::handle_dump(&meta, info);
-                human_panic::print_msg(file_path, &meta)
-                    .expect("human-panic: printing error message to console failed");
-            }));
-        }
-        Ok(_) => {}
+            // Then call human_panic
+            let file_path = human_panic::handle_dump(&meta, info);
+            human_panic::print_msg(file_path, &meta)
+                .expect("human-panic: printing error message to console failed");
+        }));
     }
 }
 
